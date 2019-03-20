cb42b0298fbb7d629b25538d9d6f98e4c9e8677f
Joerie <joerie_svd@hotmail.com>
2019-03-20 14:25:13 +0100

    initial commit

---
+++ script.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn import linear_model
+
+df = pd.read_csv(
+    "https://s3.amazonaws.com/codecademy-content/programs/data-science-path/linear_regression/honeyproduction.csv")
